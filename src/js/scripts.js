7170fbbae1a077c68b8c5521e109dfd96b5b7b29
TimurTsokov <tsokov2t1m5@gmail.com>
2018-09-07 22:00:22 +0300

    first finish

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,6 +10,50 @@ $(document).ready(function () {
         }
     });
 
+    $(function () {
+
+        $('.work-link').click(function (e) {
+
+            e.preventDefault();
+            var link = $(e.relatedTarget);
+            var content = link.data('content');
+
+            $('#exampleModalCenter .modal-body img').attr('src', $(this).find('img').attr('src'));
+
+            $("#exampleModalCenter").modal('show');
+            $('this').find('#exampleModalCenterTitle').text(content);
+
+        });
+
+        $('#exampleModalCenter .modal-body img').on('click', function () {
+            $("#exampleModalCenter").modal('hide');
+            $('this').find('#exampleModalCenterTitle').text(content);
+        });
+
+        $('#prev,#next').click(function () {
+            var src = $('#exampleModalCenter .modal-body img').attr('src');
+            var img = $('.work-link').find('img[data-img="' + src + '"]');
+            if (this.id == 'next') {
+                var nextsrc = img.parent().parent().next().find('img').attr('data-img');
+                if (!nextsrc) {
+                    var img = $('.work-link').filter(':first').find('img');
+                    nextsrc = img.attr('data-img');
+                    // console.log('new:' + nextsrc);
+                }
+                $('#exampleModalCenter .modal-body img').attr('src', nextsrc);
+            } else {
+                var prevsrc = img.parent().parent().prev().find('img').attr('data-img');
+                if (!prevsrc) {
+                    var img = $('.work-link').filter(':last').find('img');
+                    prevsrc = img.attr('data-img');
+                    // console.log('new:' + prevsrc);
+                }
+                $('#exampleModalCenter .modal-body img').attr('src', prevsrc);
+            }
+        });
+
+    });
+
     $('.navbar-nav a').on('click', function () {
         $('.navbar-nav li').removeClass('active');
         $(this).parent().addClass('active');
